52e22b779c0332488670a2a83abdba1dfeb4432d
nishakar123 <nishakar123@gmail.com>
2016-10-10 10:13:10 +0530

    Add Another SOP Statement

--- src/com/nishakar/india/Example3.java
+++ src/com/nishakar/india/Example3.java
@@ -4,6 +4,7 @@ public class Example3 {
 
 	public static void main(String[] args) {
 		System.out.println("Main Starts");
+		System.out.println("Main in Between");
 		System.out.println("Main Ends");
 	}
 }
